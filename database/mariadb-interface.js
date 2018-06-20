3c2de3a61c41bc40ff68975cf62275a1ee9ed7cc
Zane Mattingly <z@matting.ly>
2018-06-20 15:58:44 -0700

    feature: all CRUD operations implemented with MariaDB

--- database/mariadb-interface.js
+++ database/mariadb-interface.js
@@ -10,91 +10,49 @@ const connection = mysql.createConnection({
 let mariadbInterface = {};
 
 // Create
-mariadbInterface.postTimeSlot = (restaurant_id, date, time, cb) => {
+mariadbInterface.postTimeSlot = (restaurant_id, date, time, party_size, party_size_max, cb) => {
   const q = 'INSERT INTO bookings SET ?';
   const booking = {
     restaurant_id,
     date,
-    time
+    time,
+    party_size,
+    party_size_max
   };
-  connection.query(q, booking, function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-  });
+  connection.query(q, booking, (error, results, fields) => cb(error, results));
 };
 
 // Retrieval
 mariadbInterface.grabBooking = (booking_id, cb) => {
   const q = 'SELECT * FROM bookings WHERE id = ?';
-  connection.query(q, [booking_id], function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    cb(error, results); 
-  });
+  connection.query(q, [booking_id], (error, results, fields) => cb(error, results));
 };
 
-mariadbInterface.grabTimeSlotId = (restaurant_id, date, time, cb) => {
-  const q = 'SELECT id FROM bookings WHERE (restaurant_id = ? && date = ? && time = ?)';
-  connection.query(q, [restaurant_id, date, time], function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    cb(error, results);
-  });
+mariadbInterface.grabBookings = (restaurant_id, date, time, cb) => {
+  const q = `SELECT * FROM bookings WHERE (restaurant_id = ? ${ date ? '&& date = ?' : '' } ${ time ? '&& time = ?' : '' })`;
+  connection.query(q, [restaurant_id, date, time], (error, results, fields) => cb(error, results));
 };
 
 mariadbInterface.grabTimeSlots = (restaurant_id, date, cb) => {
   const q = 'SELECT * FROM bookings WHERE (restaurant_id = ? && date = ?);';
-  connection.query(q, [restaurant_id, date], function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    cb(error, results); 
-  });
+  connection.query(q, [restaurant_id, date], (error, results, fields) => cb(error, results));
 };
 
 // Update
 mariadbInterface.updateBooking = (booking_id, new_date, new_time, cb) => {
-  const q = 'UPDATE bookings SET (date = ?, time = ?) WHERE id = ?;';
-  connection.query(q, [new_date, new_time, booking_id], function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    cb(error, results); 
-  });
-};
-
-mariadbInterface.updateTimeSlot = (restaurant_id, new_date, old_date, new_time, old_time, cb) => {
-  const q = 'UPDATE bookings SET (date = ?, time = ?) WHERE (restaurant_id = ? && date = ? && time = ?);';
-  connection.query(q, [new_date, new_time, restaurant_id, old_date, old_time], function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    cb(error, results);
-  });
+  const q = 'UPDATE bookings SET date = ?, time = ? WHERE id = ?;';
+  connection.query(q, [new_date, new_time, booking_id], (error, results, fields) => cb(error, results));
 };
 
 // Destroy
-mariadbInterface.deleteBooking = (booking_id, cb) => {
+mariadbInterface.deleteBooking = (restaurant_id, booking_id, cb) => {
   const q = 'DELETE FROM bookings WHERE id = ?;';
-  connection.query(q, [booking_id], function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    cb(error, results);
-  });
+  connection.query(q, [booking_id], (error, results, fields) => cb(error, results));
 };
 
-mariadbInterface.deleteTimeSlot = (restaurant_id, date, time, cb) => {
+mariadbInterface.deleteBookingsAtTimeSlot = (restaurant_id, date, time, cb) => {
   const q = 'DELETE FROM bookings WHERE (restaurant_id = ? && date = ? && time = ?);';
-  connection.query(q, [restaurant_id, date, time], function(error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    cb(error, results);
-  });
+  connection.query(q, [restaurant_id, date, time], (error, results, fields) => cb(error, results));
 };
 
 module.exports = mariadbInterface;
